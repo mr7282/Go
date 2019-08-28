bf623ea01fd68fc7fc5ddb1be966aeef5ecbbfc2
mr7282 <mr7282@mail.ru>
2019-08-28 22:26:16 +0300

    Методы начало

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 
-	"./triangle"
+	"./figures"
 )
 
 func main() {
-	var t triangle.Triangle
+	var t figures.Triangle
+	var tz figures.Trapeze
 	//Написать свой интерфейс и создать несколько структур, удовлетворяющих ему.
 
-	t.SetCatet(14, 21)
-	t.SetAltitude(10)
-	fmt.Println(t.GetCatet())
-	fmt.Println(t.Area())
+	t.SetCatet()
+	t.SetAltitude()
+	t.GetCatet()
+	fmt.Println("Площадь треугольника =", t.Area())
+	tz.SetBasis()
+	tz.SetAltitude()
+	tz.GetBasis()
+	fmt.Println
 }
