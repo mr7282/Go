97c321ea4c55210b9d6df0c7fd96c1a4330e8372
mr7282 <mr7282@mail.ru>
2019-09-19 00:05:00 +0300

    назначение вариантов хода (не до конца)

--- chess/main.go
+++ chess/main.go
@@ -4,32 +4,57 @@ import (
 	"fmt"
 )
 
-type position struct {
+//Position - координаты позиции коня
+type Position struct {
 	x int
 	y int
 }
 
+//P - переменная позиции коня
+var vp Position
+
+//PositionMove - координаты вариантов хода коня
 type PositionMove struct {
-	x int
-	y int
+	pm1 Position
+	pm2 Position
+	pm3 Position
+	pm4 Position
+	pm5 Position
+	pm6 Position
+	pm7 Position
+	pm8 Position
 }
 
+//PM - переменная структуры вариантов хода
+var vpm PositionMove
+
+//PositionOption - возвращаемый массив координат хода коня
 var PositionOption []PositionMove
 
-func (p position) GetPosition() {
+//GetPosition - ввод коорлинат первоначального нахождения коня
+func (p *Position) GetPosition() {
 	fmt.Println("Введите позицию КОНЯ по Оси Х")
 	fmt.Scanln(&p.x)
 	fmt.Println("Введите позицию КОНЯ по Оси Y")
 	fmt.Scanln(&p.y)
 }
 
+//ChessMove - расчет координат вариантов хода коня
 func (pm PositionMove) ChessMove() {
+	if vp.x <= 7 && vp.x >= 1 && vp.y <= 6 && vp.y >= 1 {
+		vpm.pm1 = Position{x: vp.x + 1, y: vp.y + 2}
+
+	} else {
+		vpm.pm1 = Position{x: 0, y: 0}
+	}
 
 }
 
-var x = position{x: 2, y: 3}
-
 func main() {
 
-	fmt.Println(x)
+	vp.GetPosition()
+	vpm.ChessMove()
+	PositionOption = append(PositionOption, vpm)
+	fmt.Println(PositionOption)
+
 }
