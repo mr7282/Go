0cf1e5c6627e2cd0c7624ee4513eba04e4a325b1
mr7282 <mr7282@mail.ru>
2019-09-19 18:21:06 +0300

    Выполнено задание 4 (a и б)

--- chess/main.go
+++ chess/main.go
@@ -31,6 +31,9 @@ var vpm PositionMove
 //PositionOption - возвращаемый массив координат хода коня
 var PositionOption []PositionMove
 
+//Range - преобразованный шахматный ряд
+var Range []string
+
 //GetPosition - ввод коорлинат первоначального нахождения коня
 func (p *Position) GetPosition() {
 	fmt.Println("Введите позицию КОНЯ по Оси Х")
@@ -47,6 +50,60 @@ func (pm PositionMove) ChessMove() {
 	} else {
 		vpm.pm1 = Position{x: 0, y: 0}
 	}
+
+	if vp.x <= 6 && vp.x >= 1 && vp.y <= 7 && vp.y >= 1 {
+		vpm.pm2 = Position{x: vp.x + 2, y: vp.y + 1}
+
+	} else {
+		vpm.pm2 = Position{x: 0, y: 0}
+	}
+
+	if vp.x <= 6 && vp.x >= 1 && vp.y <= 8 && vp.y >= 2 {
+		vpm.pm3 = Position{x: vp.x + 2, y: vp.y - 1}
+
+	} else {
+		vpm.pm3 = Position{x: 0, y: 0}
+	}
+
+	if vp.x <= 7 && vp.x >= 1 && vp.y <= 8 && vp.y >= 3 {
+		vpm.pm4 = Position{x: vp.x + 1, y: vp.y - 2}
+
+	} else {
+		vpm.pm4 = Position{x: 0, y: 0}
+	}
+
+	if vp.x <= 8 && vp.x >= 2 && vp.y <= 8 && vp.y >= 3 {
+		vpm.pm5 = Position{x: vp.x - 1, y: vp.y - 2}
+
+	} else {
+		vpm.pm5 = Position{x: 0, y: 0}
+	}
+
+	if vp.x <= 8 && vp.x >= 3 && vp.y <= 8 && vp.y >= 2 {
+		vpm.pm6 = Position{x: vp.x - 2, y: vp.y - 1}
+
+	} else {
+		vpm.pm6 = Position{x: 0, y: 0}
+	}
+
+	if vp.x <= 8 && vp.x >= 3 && vp.y <= 7 && vp.y >= 1 {
+		vpm.pm7 = Position{x: vp.x - 2, y: vp.y + 1}
+
+	} else {
+		vpm.pm7 = Position{x: 0, y: 0}
+	}
+
+	if vp.x <= 8 && vp.x >= 2 && vp.y <= 6 && vp.y >= 1 {
+		vpm.pm8 = Position{x: vp.x - 1, y: vp.y + 2}
+
+	} else {
+		vpm.pm8 = Position{x: 0, y: 0}
+	}
+
+}
+
+//converting - преобразование массива в шахматный ряд
+func converting() {
 
 }
 
