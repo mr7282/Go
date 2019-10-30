2f2c722c4172f9caab048fe1ed51738365d993aa
mr7282 <mr7282@mail.ru>
2019-10-30 13:48:33 +0300

    Переделана программа возвращающая возможные позиции шахматного коня, получая на вход текущюю позицию.

--- chess/main.go
+++ chess/main.go
@@ -5,207 +5,114 @@ import (
 	"strconv"
 )
 
-//Position - координаты позиции коня
-type Position struct {
-	x int
-	y int
-}
-
-//P - переменная позиции коня
-var vp Position
-
-//PositionMove - координаты вариантов хода коня
-type PositionMove struct {
-	pm1 Position
-	pm2 Position
-	pm3 Position
-	pm4 Position
-	pm5 Position
-	pm6 Position
-	pm7 Position
-	pm8 Position
-}
-
-//PM - переменная структуры вариантов хода
-var vpm PositionMove
-
 //Range - преобразованный шахматный ряд
 var Range []string
 
-//X - переменная для преобразования
-var X string
+/////////////////////////////////////////// Почему не могу переместить "Position" и "Vp"
+/////////////////////////////////////////// в функцию "GetPosition"
 
-//S - переменная для итоговой строки
-var S string
-
-//I - переменная инт
-var I int
-
-//GetPosition - ввод коорлинат первоначального нахождения коня
-func (p *Position) GetPosition() {
-	fmt.Println("Введите позицию КОНЯ по Оси Х (буквы от a до h)")
-	fmt.Scanln(&S)
-	convertInput(S)
-	p.x = I
-	fmt.Println("Введите позицию КОНЯ по Оси Y")
-	fmt.Scanln(&p.y)
+//Position - координаты позиции коня
+type Position struct {
+	X int
+	Y int
 }
 
-//ChessMove - расчет координат вариантов хода коня
-func (pm PositionMove) ChessMove() {
-	if vp.x <= 7 && vp.x >= 1 && vp.y <= 6 && vp.y >= 1 {
-		vpm.pm1 = Position{x: vp.x + 1, y: vp.y + 2}
-		converting(vpm.pm1.x)
-		X = S + strconv.Itoa(vpm.pm1.y)
-		appendArray(X)
+//Vp - переменная позиции коня
+var Vp Position
 
-	} else {
-		vpm.pm1 = Position{x: 0, y: 0}
-		converting(vpm.pm1.x)
-		appendArray(S)
-	}
+//GetPosition - ввод коорлинат первоначального нахождения коня
+func GetPosition() {
 
-	if vp.x <= 6 && vp.x >= 1 && vp.y <= 7 && vp.y >= 1 {
-		vpm.pm2 = Position{x: vp.x + 2, y: vp.y + 1}
-		converting(vpm.pm2.x)
-		X = S + strconv.Itoa(vpm.pm2.y)
-		appendArray(X)
+	//S - переменная для преобразования
+	var S string
 
-	} else {
-		vpm.pm2 = Position{x: 0, y: 0}
-		converting(vpm.pm2.x)
-		appendArray(S)
-	}
-
-	if vp.x <= 6 && vp.x >= 1 && vp.y <= 8 && vp.y >= 2 {
-		vpm.pm3 = Position{x: vp.x + 2, y: vp.y - 1}
-		converting(vpm.pm3.x)
-		X = S + strconv.Itoa(vpm.pm3.y)
-		appendArray(X)
-
-	} else {
-		vpm.pm3 = Position{x: 0, y: 0}
-		converting(vpm.pm3.x)
-		appendArray(S)
-	}
-
-	if vp.x <= 7 && vp.x >= 1 && vp.y <= 8 && vp.y >= 3 {
-		vpm.pm4 = Position{x: vp.x + 1, y: vp.y - 2}
-		converting(vpm.pm4.x)
-		X = S + strconv.Itoa(vpm.pm4.y)
-		appendArray(X)
-
-	} else {
-		vpm.pm4 = Position{x: 0, y: 0}
-		converting(vpm.pm4.x)
-		appendArray(S)
-	}
-
-	if vp.x <= 8 && vp.x >= 2 && vp.y <= 8 && vp.y >= 3 {
-		vpm.pm5 = Position{x: vp.x - 1, y: vp.y - 2}
-		converting(vpm.pm5.x)
-		X = S + strconv.Itoa(vpm.pm5.y)
-		appendArray(X)
-
-	} else {
-		vpm.pm5 = Position{x: 0, y: 0}
-		converting(vpm.pm5.x)
-		appendArray(S)
-	}
-
-	if vp.x <= 8 && vp.x >= 3 && vp.y <= 8 && vp.y >= 2 {
-		vpm.pm6 = Position{x: vp.x - 2, y: vp.y - 1}
-		converting(vpm.pm6.x)
-		X = S + strconv.Itoa(vpm.pm6.y)
-		appendArray(X)
-
-	} else {
-		vpm.pm6 = Position{x: 0, y: 0}
-		converting(vpm.pm6.x)
-		appendArray(S)
-	}
-
-	if vp.x <= 8 && vp.x >= 3 && vp.y <= 7 && vp.y >= 1 {
-		vpm.pm7 = Position{x: vp.x - 2, y: vp.y + 1}
-		converting(vpm.pm7.x)
-		X = S + strconv.Itoa(vpm.pm7.y)
-		appendArray(X)
-
-	} else {
-		vpm.pm7 = Position{x: 0, y: 0}
-		converting(vpm.pm7.x)
-		appendArray(S)
-	}
-
-	if vp.x <= 8 && vp.x >= 2 && vp.y <= 6 && vp.y >= 1 {
-		vpm.pm8 = Position{x: vp.x - 1, y: vp.y + 2}
-		converting(vpm.pm8.x)
-		X = S + strconv.Itoa(vpm.pm8.y)
-		appendArray(X)
-
-	} else {
-		vpm.pm8 = Position{x: 0, y: 0}
-		converting(vpm.pm8.x)
-		appendArray(S)
-	}
-
+	fmt.Println("Введите позицию КОНЯ по Оси Х (буквы от a до h)")
+	fmt.Scanln(&S)
+	Vp.X = convertInput(S)
+	fmt.Println("Введите позицию КОНЯ по Оси Y")
+	fmt.Scanln(&Vp.Y)
 }
 
 //converting - преобразование массива в шахматный ряд
-func converting(k int) {
+func converting(k int) string {
 
 	switch k {
-	case 0:
-		S = "-"
 	case 1:
-		S = "a"
+		return "a"
 	case 2:
-		S = "b"
+		return "b"
 	case 3:
-		S = "c"
+		return "c"
 	case 4:
-		S = "d"
+		return "d"
 	case 5:
-		S = "e"
+		return "e"
 	case 6:
-		S = "f"
+		return "f"
 	case 7:
-		S = "g"
+		return "g"
 	case 8:
-		S = "h"
+		return "h"
 	}
-
+	return "-"
 }
 
-func convertInput(k string) {
+func convertInput(k string) int {
 	switch k {
 	case "a":
-		I = 1
+		return 1
 	case "b":
-		I = 2
+		return 2
 	case "c":
-		I = 3
+		return 3
 	case "d":
-		I = 4
+		return 4
 	case "e":
-		I = 5
+		return 5
 	case "f":
-		I = 6
+		return 6
 	case "g":
-		I = 7
+		return 7
 	case "h":
-		I = 8
+		return 8
 	}
+	return 0 /////////////////////////////////Как избежать такой ситуации? (не нужный return)
 }
 
 func appendArray(l string) {
 	Range = append(Range, l)
 }
 
+//ChessMove - расчет координат вариантов хода коня
+func ChessMove() {
+
+	type PositionMove struct {
+		AxisX int
+		AxisY int
+	}
+
+	ArrPM := []PositionMove{{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}}
+
+	for _, arrrange := range ArrPM {
+
+		newX := Vp.X + arrrange.AxisX
+		newY := Vp.Y + arrrange.AxisY
+
+		if newX > 0 && newX < 9 && newY > 0 && newY < 9 {
+
+			appendArray(converting(newX) + strconv.Itoa(newY))
+		} else {
+			newX = 0
+			appendArray(converting(newX))
+		}
+	}
+
+}
+
 func main() {
 
-	vp.GetPosition()
-	vpm.ChessMove()
+	GetPosition()
+	ChessMove()
 	fmt.Println(Range)
 
 }
