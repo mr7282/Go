0b2ed349569e4976f81a6a399d805bc2e84497eb
mr7282 <mr7282@mail.ru>
2020-03-02 02:57:48 +0300

    Пробуем

--- newLine course basik Go/homework-3/task-4/main.go
+++ newLine course basik Go/homework-3/task-4/main.go
@@ -3,18 +3,52 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//"io/ioutil"
+	"io/ioutil"
 )
 
 //Внести в телефонный справочник дополнительные данные. Реализовать сохранение json-файла на диске с помощью пакета ioutil при изменении данных.
 func main() {
 	telephoneBook := make(map[string]int)
+	content, err := ioutil.ReadFile("telephoneBook.json")
+	if content != nil {
+		telephoneBookJSON := json.Unmarshal(content)
 
-	telephoneBook["Грицай"] = 89261234565
-	telephoneBook["Коханов"] = 89164787489
-	telephoneBook["Иванов"] = 89254541232
-	telephoneBook["Крылов"] = 89035264574
-	telephoneBook["Лазуренко"] = 89265788987
+	}
+	telephoneBook = telephoneBookJSON
+	for {
+		fmt.Println("Введите 1 если необходимо добавить контакт\nВведите 2 если необходимо вывести телефонную книгу целиком\nВведите 3 если необходимо осуществить поиск\nВведите 4 для выхода из телефонной книги")
+		var condition string
+		fmt.Scanln(&condition)
+		switch condition {
+		case "1":
+			getNewContact(telephoneBook)
+		case "2":
+			fmt.Println(telephoneBook)
+		case "3":
+			findContact(telephoneBook)
+		case "4":
+			fmt.Println("Спасибо за пользование наше зписной ")
+			break
+		default:
+			fmt.Println("Неверно введено значение. Попробуй еще раз!")
+		}
+		if condition == "4" {
+			break
+		}
+
+		m, err := json.Marshal(telephoneBook)
+		if err != nil {
+			fmt.Println("Что то пошло не так")
+		}
+		err := ioutil.WriteFile("telephoneBook.json", m, 0644)
+
+	}
+
+	// telephoneBook["Грицай"] = 89261234565
+	// telephoneBook["Коханов"] = 89164787489
+	// telephoneBook["Иванов"] = 89254541232
+	// telephoneBook["Крылов"] = 89035264574
+	// telephoneBook["Лазуренко"] = 89265788987
 
 	// m, err := json.Marshal(telephoneBook)
 	// if err != nil {
@@ -27,4 +61,21 @@ func main() {
 	// fmt.Println(len(telephoneBook))
 	// fmt.Println(string(m))
 
+}
+
+func getNewContact(book map[string]int) {
+	fmt.Println("Ввидите Фамилию")
+	var surname string
+	fmt.Scanln(&surname)
+	fmt.Println("Введите телефонный номер")
+	var number int
+	fmt.Scanln(&number)
+	book[surname] = number
+}
+
+func findContact(book map[string]int) {
+	fmt.Println("Введите фамилию для поиска:")
+	var surname string
+	fmt.Scanln(&surname)
+	fmt.Println(book[surname])
 }
