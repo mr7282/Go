ea67b2f66a52a3f4766cce7e7836992f6d15d68d
mr7282 <mr7282@mail.ru>
2020-03-04 23:03:43 +0300

    Выполнено 4 задание

--- newLine course basik Go/homework-3/task-4/main.go
+++ newLine course basik Go/homework-3/task-4/main.go
@@ -11,12 +11,12 @@ func main() {
 	telephoneBook := make(map[string]int)
 	content, err := ioutil.ReadFile("telephoneBook.json")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Что то пошло не так при чтении файла!")
 	}
 	if content != nil {
 		err := json.Unmarshal(content, &telephoneBook)
 		if err != nil {
-			fmt.Println("Что то пошло не так2")
+			fmt.Println("Что то пошло не так при раскодирования JSON!")
 		}
 	}
 	for {
@@ -31,7 +31,17 @@ func main() {
 		case "3":
 			findContact(telephoneBook)
 		case "4":
-			fmt.Println("Спасибо за пользование наше зписной ")
+			m, err := json.Marshal(telephoneBook)
+			if len(m) != len(content) {
+				if err != nil {
+					fmt.Println("Что то пошло не так при кодировании в JSON!")
+				}
+				err = ioutil.WriteFile("telephoneBook.json", m, 0644)
+				fmt.Println("Все записи сохранены")
+			} else {
+				fmt.Println("Без сохранения")
+			}
+			fmt.Println("Спасибо за пользование наше зписной книжкой")
 			break
 		default:
 			fmt.Println("Неверно введено значение. Попробуй еще раз!")
@@ -39,35 +49,11 @@ func main() {
 		if condition == "4" {
 			break
 		}
-		
-		if 
-		m, err := json.Marshal(telephoneBook)
-		if err != nil {
-			fmt.Println("Что то пошло не так3")
-		}
-		err = ioutil.WriteFile("telephoneBook.json", m, 0644)
 
 	}
-
-	// telephoneBook["Грицай"] = 89261234565
-	// telephoneBook["Коханов"] = 89164787489
-	// telephoneBook["Иванов"] = 89254541232
-	// telephoneBook["Крылов"] = 89035264574
-	// telephoneBook["Лазуренко"] = 89265788987
-
-	// m, err := json.Marshal(telephoneBook)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// fmt.Println(len(telephoneBook))
-	// fmt.Scanln(&telephoneBook)
-	// fmt.Println(len(telephoneBook))
-	// fmt.Println(string(m))
-
 }
 
+//getNewContact - this function add new contact in telephonebook
 func getNewContact(book map[string]int) {
 	fmt.Println("Ввидите Фамилию")
 	var surname string
@@ -78,6 +64,7 @@ func getNewContact(book map[string]int) {
 	book[surname] = number
 }
 
+//findContact - find and shows telephone number related surname
 func findContact(book map[string]int) {
 	fmt.Println("Введите фамилию для поиска:")
 	var surname string
@@ -85,6 +72,7 @@ func findContact(book map[string]int) {
 	fmt.Printf("%v\n\n", book[surname])
 }
 
+//showTelephoneBook - shows all contact contained telephone book
 func showTelephoneBook(book map[string]int) {
 	for name, number := range book {
 		fmt.Printf("%v - %v\n", name, number)
