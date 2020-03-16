2ab2c854447ad721e7cfbda14e7de00d537b405b
mr7282 <mr7282@mail.ru>
2020-03-16 04:15:01 +0300

    Пробуем интерфейс

--- newLine course basik Go/homework-4/task-1/main.go
+++ newLine course basik Go/homework-4/task-1/main.go
@@ -4,35 +4,79 @@ import (
 	"fmt"
 )
 
-type product struct {
+type autopart struct {
 	id          int
 	name        string
 	price       int
 	description string
 }
 
-type order struct {
+type invoiceAutopart struct {
 	id         int
-	content    []product
+	content    []autopart
 	totalPrice int
 }
 
-func (o *order) addProduct(data product) {
+type work struct {
+	id       int
+	name     string
+	brand    string
+	price    int
+	normHour float32
+}
+
+type invoiceWork struct {
+	id         int
+	content    []work
+	totalPrice int
+}
+
+type total interface {
+	Calculation() int
+}
+
+func (i *invoiceWork) addWork(data work) {
+	i.content = append(i.content, data)
+}
+
+func (i *invoiceWork) Calculation() int {
+	for _, workPrice := range i.content {
+		i.totalPrice += workPrice.price
+	}
+	return i.totalPrice
+}
+
+func (o *invoiceAutopart) addAutoPart(data autopart) {
 	o.content = append(o.content, data)
 }
 
-func (o *order) calculation() {
-	for _, productPrice := range o.content {
-		o.totalPrice += productPrice.price
+func (o *invoiceAutopart) Calculation() int {
+	for _, AutoPartPrice := range o.content {
+		o.totalPrice += AutoPartPrice.price
 	}
+	return o.totalPrice
+}
+
+func fullPrice(calcs ...total) int {
+	res := 0
+	for _, calc := range calcs {
+		res += calc.Calculation()
+	}
+	return res
 }
 
 func main() {
-	var myOrder order
-	myOrder.id = 1
-	myOrder.addProduct(product{1, "Холодильнк", 18000, "Холодильник Samsung"})
-	myOrder.addProduct(product{2, "Стиральная машина", 12000, "Стиральная машина Wirpool"})
-	myOrder.addProduct(product{3, "Чайник", 3000, "Чайник Tefal"})
-	myOrder.calculation()
-	fmt.Println(myOrder)
+	var myInvoiceAutopart invoiceAutopart
+	myInvoiceAutopart.id = 1
+	myInvoiceAutopart.addAutoPart(autopart{1, "Водяная помпа", 18000, "Audi, Skoda, VW"})
+	myInvoiceAutopart.addAutoPart(autopart{2, "Распредвал TRW", 12000, "Audi, Skoda, VW"})
+	myInvoiceAutopart.addAutoPart(autopart{3, "Щетки стеклоочистителя Bosch 60\"", 3000, "Multibrand"})
+	myInvoiceAutopart.Calculation()
+	var myInvoiceWork invoiceWork
+	myInvoiceWork.id = 1
+	myInvoiceWork.addWork(work{1, "Замена водяной помпы", "Skoda", 6000, 3.5})
+	myInvoiceWork.addWork(work{2, "Замена распредвала", "Skoda", 8000, 4.5})
+	myInvoiceWork.addWork(work{3, "Замена щеток стеклоочестителч", "Skoda", 1000, 0.5})
+	myInvoiceWork.Calculation()
+	fmt.Println(myInvoiceAutopart, myInvoiceWork, fullPrice())
 }
