35e4b617a2058aac7f671f39eefb8eb16842b7f2
mr7282 <mr7282@mail.ru>
2020-03-21 21:46:34 +0300

    Выполнено 1 задание

--- newLine course basik Go/homework-4/task-1/main.go
+++ newLine course basik Go/homework-4/task-1/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+//Написать свой интерфейс и создать несколько структур, удовлетворяющих ему.
+
 type autopart struct {
 	id          int
 	name        string
@@ -12,9 +14,8 @@ type autopart struct {
 }
 
 type invoiceAutopart struct {
-	id         int
-	content    []autopart
-	totalPrice int
+	id      int
+	content []autopart
 }
 
 type work struct {
@@ -26,9 +27,8 @@ type work struct {
 }
 
 type invoiceWork struct {
-	id         int
-	content    []work
-	totalPrice int
+	id      int
+	content []work
 }
 
 type total interface {
@@ -40,10 +40,11 @@ func (i *invoiceWork) addWork(data work) {
 }
 
 func (i *invoiceWork) Calculation() int {
+	totalPrice := 0
 	for _, workPrice := range i.content {
-		i.totalPrice += workPrice.price
+		totalPrice += workPrice.price
 	}
-	return i.totalPrice
+	return totalPrice
 }
 
 func (o *invoiceAutopart) addAutoPart(data autopart) {
@@ -51,10 +52,11 @@ func (o *invoiceAutopart) addAutoPart(data autopart) {
 }
 
 func (o *invoiceAutopart) Calculation() int {
+	totalPrice := 0
 	for _, AutoPartPrice := range o.content {
-		o.totalPrice += AutoPartPrice.price
+		totalPrice += AutoPartPrice.price
 	}
-	return o.totalPrice
+	return totalPrice
 }
 
 func fullPrice(calcs ...total) int {
@@ -71,12 +73,10 @@ func main() {
 	myInvoiceAutopart.addAutoPart(autopart{1, "Водяная помпа", 18000, "Audi, Skoda, VW"})
 	myInvoiceAutopart.addAutoPart(autopart{2, "Распредвал TRW", 12000, "Audi, Skoda, VW"})
 	myInvoiceAutopart.addAutoPart(autopart{3, "Щетки стеклоочистителя Bosch 60\"", 3000, "Multibrand"})
-	myInvoiceAutopart.Calculation()
 	var myInvoiceWork invoiceWork
 	myInvoiceWork.id = 1
 	myInvoiceWork.addWork(work{1, "Замена водяной помпы", "Skoda", 6000, 3.5})
 	myInvoiceWork.addWork(work{2, "Замена распредвала", "Skoda", 8000, 4.5})
 	myInvoiceWork.addWork(work{3, "Замена щеток стеклоочестителч", "Skoda", 1000, 0.5})
-	myInvoiceWork.Calculation()
-	fmt.Println(myInvoiceAutopart, myInvoiceWork, fullPrice())
+	fmt.Println(myInvoiceAutopart, "\n", myInvoiceAutopart.Calculation(), "\n", myInvoiceWork, "\n", myInvoiceWork.Calculation(), "\n", fullPrice(&myInvoiceWork, &myInvoiceAutopart))
 }
