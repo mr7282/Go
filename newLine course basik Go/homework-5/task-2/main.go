fa6d2d2634e9ab97b15950fd4f4c69a00df81491
mr7282 <mr7282@mail.ru>
2020-04-11 23:40:35 +0300

    Start task 5

---
+++ newLine course basik Go/homework-5/task-2/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"path/filepath"
+)
+
+func main() {
+	innnerPath := os.Args[1]
+	innerGrep := os.Args[2]
+	// var list []string
+	openDir, err := os.Open(innnerPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer openDir.Close()
+
+	filepath.Walk(innnerPath, func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	})
+
+
+	// readDir, err := openDir.Readdir(-1)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// for _, rd := range readDir {
+	// 	list = append(list, rd.Name())
+	// }
+	fmt.Println(innerGrep)
+	// fmt.Println(list)
+
+}
